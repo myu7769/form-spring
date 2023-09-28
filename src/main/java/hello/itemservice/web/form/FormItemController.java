d39125c8fb0a4cdc9554ea1531ca8b77cbcf1b85
myung7769 <myung7769@gmail.com>
2023-09-28 19:34:39 +0900

    멀티 체크박스에 타임 리프 적용

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,8 +21,19 @@ public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+    @ModelAttribute("regions") // 모든 FormItemController 리턴에 regions 이라는 모델이 포함된다.
+    public Map<String, String> regions(){
+        Map<String, String> regions = new LinkedHashMap<>();
+
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
     @GetMapping
     public String items(Model model) {
+
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "form/items";
@@ -29,13 +42,18 @@ public class FormItemController {
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
+
+
         model.addAttribute("item", item);
+
         return "form/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
+
+
         return "form/addForm";
     }
 
